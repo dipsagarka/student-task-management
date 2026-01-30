f092bd6779de4c5737f23e7a40f11f2863dd8337
Dip Sagarka <dipsagarka1204@gmail.com>
2026-01-30 13:49:53 +0530

    register page added

---
+++ src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ".././index.css";
+
+const Register = () => {
+  return (
+    <>
+      <div className="form-container">
+        <h1>This is register page</h1>
+      </div>
+    </>
+  );
+};
+
+export default Register;
