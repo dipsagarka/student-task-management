dd2a8d356c6d699ea5d5806a9fc219d67bb933ec
Dip Sagarka <dipsagarka1204@gmail.com>
2026-02-05 10:16:21 +0530

    files added

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,33 +1,55 @@
-import React from 'react'
+import React from "react";
 
-const TaskList = () => {
+const TaskList = ({ tasks }) => {
   return (
-    <div className='task-grid'>
-      
-      {/*Task card 1 */}
-      <div className="task-card" style={{position: 'relative'}}>
-         <h3>Complete React  Assignment</h3>
-         <p>Finish task Manager UI and styling</p>
+    <div className="task-grid">
+      {tasks.map((task) => (
+        <div
+          key={task.id} // ✅ unique key
+          className="task-card"
+          style={{ position: "relative" }}
+        >
+          <h3>{task.title}</h3>
+          <p>{task.description}</p>
 
-         <div className="task-meta">
-            <span>Due: 2026-02-10</span>
-            <span className="priority-badge priority-high">High</span>
-         </div>
+          <div className="task-meta">
+            <span>Due: {task.duedate}</span>
+            <span
+              className={`priority-badge priority-${task.priority.toLowerCase()}`}
+            >
+              {task.priority}
+            </span>
+          </div>
 
-         <div className="task-actions">
-            <button className="btn-icon" style={{background: '#00d2ff'}} title='Edit Task'>
-            ✏️
+          <div className="task-actions">
+            <button
+              className="btn-icon"
+              style={{ background: "#00d2ff" }}
+              title="Edit Task"
+            >
+              ✏️
             </button>
 
-            <button className="btn-icon" style={{background: '#00b894'}} title='Mark Complete'>
-            ✔️
+            <button
+              className="btn-icon"
+              style={{ background: "#00b894" }}
+              title="Mark Complete"
+            >
+              ✔️
             </button>
 
-            <button className="btn-icon" style={{background: '#fff416c'}} title='Delete Task'>🗑️</button>
-         </div>
-      </div>
+            <button
+              className="btn-icon"
+              style={{ background: "red" }}
+              title="Delete Task"
+            >
+              🗑️
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,46 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar'
-import { useNavigate } from 'react-router-dom';
-import TaskList from '../components/TaskList';
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import TaskList from "../components/TaskList";
 
 const Dashboard = () => {
+  const Navigate = useNavigate();
 
-  const Navigate = useNavigate()
-  
   const [tasks, setTasks] = useState([]);
-
 
   const fetchData = async () => {
     try {
       const response = await fetch("http://localhost:3000/tasks");
       const data = await response.json();
       setTasks(data);
-  } catch (error) {
-    console.log(error)
-  }
-}
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     const loadTasks = async () => {
       await fetchData();
     };
     loadTasks();
-  }, [])
+  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem("loginData");
     // localStorage.removeItem("authData");
     // localStorage.clear();
-    Navigate('/Login')
-  }
+    Navigate("/Login");
+  };
 
   return (
     <div>
-      <Navbar onLogout={handleLogout}/>
+      <Navbar onLogout={handleLogout} />
       <h1>MY TASKS</h1>
-      <TaskList tasks={tasks}/>
+      <TaskList tasks={tasks} />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
