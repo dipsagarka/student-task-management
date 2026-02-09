b545a13a534c518fd3671ae17b77f84fbe1016ef
Dip Sagarka <dipsagarka1204@gmail.com>
2026-02-09 13:35:49 +0530

    final project complated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "../index.css";
 
-function Navbar({onLogout}) {
+function Navbar({ title ,onLogout ,onAddTaskBtnClick , isFormOpen}) {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <h1>Task Manager</h1>
+        <h1>{title}</h1>
       </div>
 
       <div className="navbar-actions">
-        <button className="btn-primary">Add Task</button>
+        <button className={isFormOpen ? "btn-secondary" : "btn-primary"} onClick={onAddTaskBtnClick}>
+          {isFormOpen ? 'Close' : 'Add Task'}</button>
 
         <button className="btn-secondary" onClick={onLogout}>Logout</button>
       </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,69 +1,78 @@
-import React, { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
-import { useNavigate } from "react-router-dom";
-import TaskForm from "../components/TaskForm";
-import TaskList from "../components/TaskList";
+  import React, { useEffect, useState } from "react";
+  import Navbar from "../components/Navbar";
+  import { useNavigate } from "react-router-dom";
+  import TaskForm from "../components/TaskForm";
+  import TaskList from "../components/TaskList";
 
-const Dashboard = () => {
-  const navigate = useNavigate();
+  const Dashboard = () => {
+    const navigate = useNavigate();
 
-  const [tasks, setTasks] = useState([]);
-  const [editTask, setEditTask] = useState(null);
+    const [tasks, setTasks] = useState([]);
+    const [editTask, setEditTask] = useState(null);
+    const [showForm, setShowForm] = useState(false);
 
-  // 🔄 fetch all tasks
-  const fetchTasks = async () => {
-    try {
-      const response = await fetch("http://localhost:3000/tasks");
-      const data = await response.json();
-      setTasks(data);
-    } catch (error) {
-      console.error("Fetch error:", error);
-    }
-  };
-
-   useEffect(() => {
-    const loadTasks = async () => {
-      await fetchTasks();
+    // 🔄 fetch all tasks
+    const fetchTasks = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/tasks");
+        const data = await response.json();
+        setTasks(data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
     };
-    loadTasks();
-  }, []);
 
-  // add or update task in UI
-  const handleTaskSave = (task, isEdit) => {
-    if (isEdit) {
-      setTasks(tasks.map(t => (t.id === task.id ? task : t)));
-    } else {
-      setTasks([...tasks, task]);
-    }
+    useEffect(() => {
+      const loadTasks = async () => {
+        await fetchTasks();
+      };
+      loadTasks();
+    }, []);
+
+    // add or update task in UI
+    const handleTaskSave = (task, isEdit) => {
+      if (isEdit) {
+        setTasks(tasks.map(t => (t.id === task.id ? task : t)));
+      } else {
+        setTasks([...tasks, task]);
+      }
+    };
+
+    // 🚪 logout
+    const handleLogout = () => {
+      localStorage.removeItem("loginData");
+      navigate("/Login");
+    };
+
+    return (
+      <div>
+        <Navbar 
+          title="Task Managment"
+          onAddTaskBtnClick={() => setShowForm(!showForm)}
+          isFormOpen={showForm}
+          onLogout={handleLogout} 
+
+        />
+
+        {/* ADD / EDIT FORM */}
+        {showForm && (
+        <TaskForm
+          editTask={editTask}
+          setEditTask={setEditTask}
+          onTaskSaved={handleTaskSave}
+        />
+        )}
+
+        <h1 style={{ margin: "20px 0" }}>MY TASKS</h1>
+
+        {/* TASK LIST */}
+        <TaskList
+          tasks={tasks}
+          setTasks={setTasks}
+          onEdit={(task) => setEditTask(task)}
+        />
+      </div>
+    );
   };
 
-  // 🚪 logout
-  const handleLogout = () => {
-    localStorage.removeItem("loginData");
-    navigate("/Login");
-  };
-
-  return (
-    <div>
-      <Navbar onLogout={handleLogout} />
-
-      {/* ADD / EDIT FORM */}
-      <TaskForm
-        editTask={editTask}
-        setEditTask={setEditTask}
-        onTaskSaved={handleTaskSave}
-      />
-
-      <h1 style={{ margin: "20px 0" }}>MY TASKS</h1>
-
-      {/* TASK LIST */}
-      <TaskList
-        tasks={tasks}
-        setTasks={setTasks}
-        onEdit={(task) => setEditTask(task)}
-      />
-    </div>
-  );
-};
-
-export default Dashboard;
+  export default Dashboard;
